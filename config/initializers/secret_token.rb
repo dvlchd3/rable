478bc2305ebd1833b195d1e2082569a80385421e
Dan Buhrman <dan.buhrman@gmail.com>
2013-01-02 20:06:28 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rable::Application.config.secret_token = '959b3428e0e652ad8f2eda5a0a23fb7a8d78dac1ed2269da4d737d77de22dfe9ab931f4d17579af4e05a88231cbe03650217032d2abf1a7ba17a629f54e38da2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Rable::Application.config.session_store :cookie_store, key: '_rable_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Rable::Application.config.session_store :active_record_store
